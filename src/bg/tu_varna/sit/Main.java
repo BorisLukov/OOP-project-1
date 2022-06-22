cc84748e523f32897771ee8a4d7cd9dfdea6dc8f
BorisLukov <100590073+BorisLukov@users.noreply.github.com>
2022-06-22 11:47:31 +0300

    methods

--- src/bg/tu_varna/sit/Main.java
+++ src/bg/tu_varna/sit/Main.java
@@ -1,10 +1,12 @@
 package bg.tu_varna.sit;
 
+import java.io.IOException;
 import java.util.Scanner;
 
 public class Main {
 
-    public static void main(String[] args) {
+    public static void main(String[] args) throws IOException {
+        RoomOperations roomOperations = new RoomOperations();
         System.out.println("Select your option:");
         System.out.println("Type help to see the supported commands.");
         Scanner scanner = new Scanner(System.in);
@@ -12,72 +14,109 @@ public class Main {
 
         do {
             op = scanner.nextLine();
-            switch (op) {
-                case "open":
-                    scanner.nextLine();
-                    break;
-                case "save":
-                    scanner.nextLine();
+            if (!roomOperations.isStatus()) {
+                switch (op) {
+                    case "open":
+                        roomOperations.open();
+                        scanner.nextLine();
+                        break;
+                    case "help":
+                        System.out.println("Available commands:");
+                        System.out.println("open <file> opens the file");
+                        System.out.println("save saves and closes the current file");
+                        System.out.println("saveas <file> saves and closes the current file in <file>");
+                        System.out.println("close closes the opened file without saving the current progress");
+                        System.out.println("checkin registers you in the room");
+                        System.out.println("availability shows a list of free rooms");
+                        System.out.println("checkout leaves the room");
+                        System.out.println("report shows a list of the room usage");
+                        System.out.println("find finds a suitable room with a number of beds");
+                        System.out.println("find! closes the opened file without saving the current progress");
+                        System.out.println("unavailable makes a room unavailable for a specific time");
+                        System.out.println("exit exits the program");
+                        scanner.nextLine();
+                        break;
+                    case "exit":
+                        scanner.nextLine();
+                        break;
+                    default:
+                        System.out.println("Invalid option");
+                        break;
+                }
+            } else {
+                switch (op) {
+                    case "open":
+                        roomOperations.open();
+                        scanner.nextLine();
+                        break;
+                    case "close":
+                        roomOperations.close();
+                        scanner.nextLine();
+                    case "save":
 
-                    break;
-                case "saveas":
-                    scanner.nextLine();
+                        scanner.nextLine();
 
-                    break;
-                case "exit":
-                    scanner.nextLine();
+                        break;
+                    case "saveas":
+                        scanner.nextLine();
 
-                    break;
-                case "help":
-                    System.out.println("Available commands:");
-                    System.out.println("open <file> opens the file");
-                    System.out.println("save saves and closes the current file");
-                    System.out.println("saveas <file> saves and closes the current file in <file>");
-                    System.out.println("close closes the opened file without saving the current progress");
-                    System.out.println("checkin registers you in the room");
-                    System.out.println("availability shows a list of free rooms");
-                    System.out.println("checkout leaves the room");
-                    System.out.println("report shows a list of the room usage");
-                    System.out.println("find finds a suitable room with a number of beds");
-                    System.out.println("find! closes the opened file without saving the current progress");
-                    System.out.println("unavailable makes a room unavailable for a specific time");
-                    System.out.println("exit exits the program");
-                    scanner.nextLine();
+                        break;
+                    case "exit":
+                        scanner.nextLine();
 
-                    break;
-                case "checkin":
+                        break;
+                    case "help":
+                        System.out.println("Available commands:");
+                        System.out.println("open <file> opens the file");
+                        System.out.println("save saves and closes the current file");
+                        System.out.println("saveas <file> saves and closes the current file in <file>");
+                        System.out.println("close closes the opened file without saving the current progress");
+                        System.out.println("checkin registers you in the room");
+                        System.out.println("availability shows a list of free rooms");
+                        System.out.println("checkout leaves the room");
+                        System.out.println("report shows a list of the room usage");
+                        System.out.println("find finds a suitable room with a number of beds");
+                        System.out.println("find! closes the opened file without saving the current progress");
+                        System.out.println("unavailable makes a room unavailable for a specific time");
+                        System.out.println("exit exits the program");
 
-                    scanner.nextLine();
 
-                    break;
-                case "availability":
-                    scanner.nextLine();
+                        break;
+                    case "checkin":
 
-                    break;
-                case "checkout":
-                    scanner.nextLine();
+                        scanner.nextLine();
 
-                    break;
-                case "report":
-                    scanner.nextLine();
+                        break;
+                    case "availability":
+                        scanner.nextLine();
 
-                    break;
-                case "find":
-                    scanner.nextLine();
+                        break;
+                    case "checkout":
+                        scanner.nextLine();
 
-                    break;
-                case "find!":
-                    scanner.nextLine();
+                        break;
+                    case "report":
+                        scanner.nextLine();
 
-                    break;
-                case "unavailable":
-                    scanner.nextLine();
+                        break;
+                    case "find":
+                        scanner.nextLine();
 
-                    break;
-                default:
-                    System.out.println("Invalid option");
-                    break;
+                        break;
+                    case "find!":
+                        scanner.nextLine();
+
+                        break;
+                    case "unavailable":
+                        scanner.nextLine();
+
+                        break;
+                    default:
+                        System.out.println("Invalid option");
+                        break;
+                }
             }
-        }while(!op.equals("exit"));
+        }while (!op.equals("exit")) ;
+
     }
 }
--- src/bg/tu_varna/sit/RoomOperations.java
+++ src/bg/tu_varna/sit/RoomOperations.java
@@ -1,5 +1,8 @@
 package bg.tu_varna.sit;
 
+import java.io.File;
+import java.io.FileReader;
+import java.io.IOException;
 import java.text.ParseException;
 import java.text.SimpleDateFormat;
 import java.util.ArrayList;
@@ -7,7 +10,40 @@ import java.util.Scanner;
 
 public class RoomOperations {
     private RoomDetails roomDetails;
+
+    public boolean isStatus() {
+        return status;
+    }
+
+    public void setStatus(boolean status) {
+        this.status = status;
+    }
+
+    private boolean status;
     Scanner scanner = new Scanner(System.in);
+
+    public void open() throws IOException {
+        try {
+            File file = new File("Hotel.json");
+            if (file.createNewFile()) {
+                System.out.println("File created and opened: " + file.getName());
+            } else {
+                System.out.println("File successfully opened.");
+            }
+        } catch (IOException e) {
+            System.out.println("File couldn't open.");
+            e.printStackTrace();
+        }
+        FileReader fileReader = new FileReader("Hotel.json");
+        setStatus(true);
+        fileReader.close();
+    }
+
+    public void close() throws IOException {
+        FileReader fileReader = new FileReader("Hotel.json");
+        fileReader.close();
+        setStatus(false);
+    }
 
     private ArrayList<RoomDetails> roomOperations = new ArrayList<>();
 
@@ -34,12 +70,29 @@ public class RoomOperations {
         }
     }
 
-    public void availability(){
-
+    public void availability() throws ParseException {
+        ArrayList<RoomDetails> availableRooms = new ArrayList();
+        SimpleDateFormat sdf = new SimpleDateFormat("yyyy-MM-dd");
+        String op = scanner.nextLine();
+        for (RoomDetails roomOperation : roomOperations) {
+            if(sdf.parse(op).before(roomDetails.getDateFrom()) && sdf.parse(op).after(roomDetails.getDateTo())){
+                availableRooms.add(roomDetails);
+            }
+        }
+        for (RoomDetails availableRoom : availableRooms) {
+            System.out.println(roomDetails);
+        }
     }
 
     public void checkout(){
-
+        String op = scanner.nextLine();
+        for (RoomDetails roomOperation : roomOperations) {
+            if(roomDetails.getRoomNumber() ==  Integer.parseInt(op)){
+                roomDetails.setNote("Available");
+                roomOperation.setDateFrom(null);
+                roomOperation.setDateTo(null);
+            }
+        }
     }
 
     public void report(){
